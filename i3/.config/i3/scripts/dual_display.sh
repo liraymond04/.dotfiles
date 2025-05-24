99e429a794ccd60e3156606b59e840fcb8f9718a
Raymond Li <li.raymond04@gmail.com>
2025-05-24 16:59:51 -0600

    Add scripts to handle dual monitors

---
+++ i3/.config/i3/scripts/dual_display.sh
@@ -0,0 +1,3 @@
+xrandr --dpi 192 --fb 5360x1800 --output DP-2 --mode 1400x900 --scale 2x2 --pos 0x0 --panning 2800x1800+0+0 --output eDP-1 --mode 2560x1600 --pos 2800x0
+
+feh --bg-scale /usr/share/wallpapers/MilkyWay/contents/images/5120x2880.png
---
+++ i3/.config/i3/scripts/single_display.sh
@@ -0,0 +1,3 @@
+xrandr --dpi 192 --output DP-2 --off --output eDP-1 --mode 2560x1600 --pos 0x0
+
+feh --bg-scale /usr/share/wallpapers/MilkyWay/contents/images/5120x2880.png
